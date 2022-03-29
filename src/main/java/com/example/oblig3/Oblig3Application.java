882316ad98acfda3361bb98b8563cd74d0c17125
Leif Esten Grimstad Kielland <leifkielland@runbox.com>
2022-03-29 10:59:17 +0200

    Initial commit

---
+++ src/main/java/com/example/oblig3/Oblig3Application.java
@@ -0,0 +1,13 @@
+package com.example.oblig3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oblig3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oblig3Application.class, args);
+    }
+
+}
